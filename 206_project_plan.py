9a23a42747ea11c01a6547ca4d79bdfa4d587654
Daniel Eilender <danlouis@umich.edu>
2017-04-18 17:17:40 -0400

    New caching functions and movie tuple list

--- 206_project_plan.py
+++ 206_project_plan.py
@@ -24,10 +24,14 @@ auth.set_access_token(access_token, access_token_secret)
 # Set up library to grab stuff from twitter with your authentication, and return it in a JSON format 
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
-movie_1 = input('Please enter a movie title')
-movie_2 = input('Please enter a second movie title')
-movie_3 = input('Please enter a third movie title')
-movie_data_list =[]
+movie_1 = 'Gladiator'
+movie_2 = 'Frozen'
+movie_3 = 'Avatar'
+movie_list = [] # making a list of movie titles
+movie_list.append(movie_1)
+movie_list.append(movie_2)
+movie_list.append(movie_3)
+
 
 CACHE_FNAME = "SI206_final_project_cache.json"
 # Put the rest of your caching setup here:
@@ -36,61 +40,53 @@ try:
 	cache_contents = cache_file.read()
 	cache_file.close()
 	CACHE_DICTION = json.loads(cache_contents)
-	cache_file.close()
 except:
 	CACHE_DICTION = {}
 
 
-def getwithcaching(movie_title):
+def getwithcaching(movie_title): # function to either get movie data from cache or requests movie adata using title
 	BASE_URL='http://www.omdbapi.com?'
-	response = request.get(BASE_URL, params={'t':movie_title})
-	if movie_title in CACHE_DICTION_text:
-		print 'using cache'
-		response_text=CACHE_DICTION_text[movie_title]
+	if movie_title in CACHE_DICTION:
+		print ('using cache')
+		response_text=CACHE_DICTION[movie_title]
 	else:
-		print 'fetching'
+		print ('fetching')
+		response = requests.get(BASE_URL, params={'t':movie_title})
+		CACHE_DICTION[movie_title] = response.text
+		response_text = response.text
 		
-		CACHE_DICTION_text[movie_title]= response.text
-		response_text=response.text
-
-		cache_file=open(CACHE_FNAME, "w")
-		cache_file.write(json.dumps(CACHE_DICTION_text))
+		# print (type(response_text))
+		cache_file = open(CACHE_FNAME, "w")
+		cache_file.write(json.dumps(CACHE_DICTION))
 		cache_file.close()
-	return response_text
+	return (json.loads(response_text))
 
+movie_data_list =[] # Making a list of movie data dictionarys
+for movie in movie_list:
+	data = getwithcaching(movie)
+	movie_data_list.append(data)
 
-a = getwithcaching(movie_1)
-b = getwithcaching(movie_2)
-c = getwithcaching(movie_3)
-movie_data_list.append(a)
-movie_data_list.append(b)
-movie_data_list.append(c)
+# print (movie_data_list[0])
+# for movie in movie_data_list:
+# 	print (movie_data_list)
 # def get_user_tweets(input_word):
 
-# 	unique_identifier = 'twitter_{}'.format(input_word)
-# 	if unique_identifier in CACHE_DICTION: # if it is...
-# 		return CACHE_DICTION[unique_identifier] # grab the data from the cache!
-# 	else:
-# 		results = api.search(q = input_word)
-# 		CACHE_DICTION[unique_identifier]=results
-# 		f=open(CACHE_FNAME, "w")
-# 		f.write(json.dumps(CACHE_DICTION, indent = 2))
-# 		f.close()
-# 		twitter_results = CACHE_DICTION[unique_identifier]
-# 		return CACHE_DICTION[unique_identifier]
 
 
-class Movie():
+class Movie(object): # Movie class that pulls data from movie dictionaries when making an instance
 	def __init__(self, movie_data):
 		self.title = movie_data['Title']
 		self.release_year = movie_data['Year']
 		self.plot = movie_data['Plot']
-		self.tomatoMeter = movie_data['tomatoMeter'] 
-		self.tomatoUserMeter = movie_data['tomatoUserMeter']
+		self.tomatoCriticMeter_string = movie_data["Metascore"]
+		self.tomatoCriticMeter = int(self.tomatoCriticMeter_string) 
+		self.tomatoUserMeter_string = movie_data['Ratings'][1]['Value'][:1]
+		self.tomatoUserMeter = int(self.tomatoUserMeter_string)
 		self.rated = movie_data['Rated'].encode('utf-8')  
-		self.id = movie_data['imbdID']
+		self.id = movie_data['imdbID']
 		self.director = movie_data['Director']
-		self.imbd_rating = movie_data['imbdRating']
+		self.imdb_rating = movie_data['imdbRating']
+		self.movie_data = movie_data
 
 	def rating(self):
 		if self.rated =="NC-17":
@@ -107,22 +103,22 @@ class Movie():
 			return self.title + " is unrated, viewers should watch at their own discretion."    
 
 	def getMovieAudience(self):
-		if self.tomatoMeter > self.tomatoUserMeter:
+		if self.tomatoCriticMeter > self.tomatoUserMeter:
 			return "The Critics like it more"
-		elif self.tomatoMeter < self.tomatoUserMeter: 
+		elif self.tomatoCriticMeter < self.tomatoUserMeter: 
 			return "The Audience likes it more"
-		elif self.tomatoMeter == self.tomatoUserMeter:    
+		elif self.tomatoCriticMeter == self.tomatoUserMeter:    
 			return "Critics and Audience like it the same"
 		else:	
 			return "Oops thats not a movie!"	
 	def movieAppeal(self):
-		if self.tomatoMeter + self.tomatoUserMeter > 180:
+		if self.tomatoCriticMeter + self.tomatoUserMeter > 180:
 			return "This movie is very highly acclaimed"
-		elif self.tomatoMeter + self.tomatoUserMeter > 140:
+		elif self.tomatoCriticMeter + self.tomatoUserMeter > 140:
 			return "This movie was fairly well recieved"	
-		elif self.tomatoMeter + self.tomatoUserMeter > 100:
+		elif self.tomatoCriticMeter + self.tomatoUserMeter > 100:
 			return "This movie had mixed reviews"
-		elif self.tomatoMeter + self.tomatoUserMeter < 100:
+		elif self.tomatoCriticMeter + self.tomatoUserMeter < 100:
 			return "This movie was not recieved well"
 		else:
 			return "Oops, thats not a movie!"					
@@ -131,22 +127,29 @@ class Movie():
 		return 'The movie {} came out in {}. The plot is: {}'.format(self.title, self.release_year, self.plot)
       
 	def num_of_languages(self):
-		all_languages = movie_data['Language'].split(",")
+		all_languages = self.movie_data['Language'].split(',')
 		return len(all_languages)
-		
+	def get_actors(self):
+		list_of_actors = self.movie_data['Actors'].split(',')
+		return (list_of_actors)	
 list_of_movie_instances = []
 
-first_movie_instance = Movie[movie_data_list[0]]
-second_movie_instance = Movie[movie_data_list[1]]
-third_movie_instance = Movie[movie_data_list[2]]
-list_of_movie_instances.append(first_movie_instance)
-list_of_movie_instances.append(second_movie_instance)
-list_of_movie_instances.append(third_movie_instance)
+for movie in movie_data_list: # Using a for loop to run each movie dictionary into the class Movie and make an instance. Then appending these instances into a list
+	movie_instance = Movie(movie)
+	list_of_movie_instances.append(movie_instance)
+
+
+movie_tuple_list = []
+
+for movie_instance in list_of_movie_instances: # This block of code is creating a list of three movie tuples containing data on three different movies
+	new_tuple = (movie_instance.id, movie_instance.title, movie_instance.plot, movie_instance.rated, movie_instance.director, movie_instance.imdb_rating, movie_instance.num_of_languages(), movie_instance.get_actors(), movie_instance.getMovieAudience())
+	movie_tuple_list.append(new_tuple)
+
+	
 
 
 
-
-
+#************TWITTER**************************************************************
 
 
 twitter_cache_file = 'twitter_cache_file.json'
@@ -169,7 +172,19 @@ else:
 
 
 
+def get_tweetdata_with_caching(input_word):
 
+	unique_identifier = input_word
+	if unique_identifier in twitter_cache_dictionary: # if it is...
+		twitter_results = twitter_cache_dictionary[unique_identifier] # grab the data from the cache!
+	else:
+		twitter_results = api.search(q = unique_identifier)
+		twitter_cache_dictionary[unique_identifier] = twitter_results
+		f=open(twitter_cache_file, "w")
+		f.write(json.dumps(twitter_cache_dictionary))
+		f.close()
+		
+		return twitter_results['statuses']
 
 
 
@@ -205,63 +220,63 @@ else:
 
 
 
-conn = sqlite3.connect('Final_Project.db')
-cur = conn.cursor()
+# conn = sqlite3.connect('Final_Project.db')
+# cur = conn.cursor()
 
-cur.execute('DROP TABLE IF EXISTS Tweets')
-cur.execute('DROP TABLE IF EXISTS Users')
-cur.execute('DROP TABLE IF EXISTS Movies')
-
-
-
-
-table_spec = 'CREATE TABLE IF NOT EXISTS Tweets(tweet_id TEXT PRIMARY KEY, text TEXT, user_id TEXT, title TEXT, favorites INTEGER, retweets INTEGER,)'
-cur.execute(table_spec)
-
-table_spec = 'CREATE TABLE IF NOT EXISTS Users(user_id TEXT PRIMARY KEY, screen_name TEXT, num_favs INTEGER,)'
-cur.execute(table_spec)
-
-table_spec = 'CREATE TABLE IF NOT EXISTS Movies(movie_id TEXT PRIMARY KEY, title TEXT, director TEXT, num_of_languages INTEGER, imbd_rating INTEGER, top_billed TEXT)'
-cur.execute(table_spec)
-conn.commit()
+# cur.execute('DROP TABLE IF EXISTS Tweets')
+# cur.execute('DROP TABLE IF EXISTS Users')
+# cur.execute('DROP TABLE IF EXISTS Movies')
 
 
 
 
+# table_spec = 'CREATE TABLE IF NOT EXISTS Tweets(tweet_id TEXT PRIMARY KEY, text TEXT, user_id TEXT, title TEXT, favorites INTEGER, retweets INTEGER)'
+# cur.execute(table_spec)
 
+# table_spec = 'CREATE TABLE IF NOT EXISTS Users(user_id TEXT PRIMARY KEY, screen_name TEXT, num_favs INTEGER)'
+# cur.execute(table_spec)
+
+# table_spec = 'CREATE TABLE IF NOT EXISTS Movies(movie_id TEXT PRIMARY KEY, title TEXT, director TEXT, num_of_languages INTEGER, imbd_rating INTEGER, top_billed TEXT)'
+# cur.execute(table_spec)
+# conn.commit()
+
+
+
+
+# conn.close()
 
 # Write your test cases here.
 
 # Attempt is an "instance of a movie"
 
-class Tests(unittest.TestCase):
-	def test_1(self):
-		self.assertEqual(type(attempt.movieAppeal()), type('a'), "testing type of return value of movieAppeal method in the Movie Class")
-	def test_2(self):
-		self.assertEqual(attempt.movieAppeal(), 'This movie is very highly acclaimed', 'Testing the return value of movieAppeal method')	
-	def test_3(self):
-		self.assertEqual(attempt.title, movie_title, 'testting side effect of the consturctor to see if the instance variable is correct')
-	def test_4(self):
-		self.assertEqual(attempt.rated, "PG", 'Testing the side effect of the constructor to see if if the instance variable self.rated returns the correct rating')
-	def test_5(self):
-		self.assertEqual(attempt.getMovieAudience(), 'The Audience likes it more', 'Testing the return value of the getMovideAudience method')
-	def test_6(self):
-		conn = sqlite3.connect('Final_project.db')
-		cur = conn.cursor()
-		cur.execute('SELECT * FROM Users');
-		result = cur.fetchall()
-		self.assertTrue(len(result[1])==3,"Testing that there are 3 columns in the Users database")
-		conn.close()
-	def test_7(self):
-		self.assertTrue(Len(list_of_movie_instances) >= 3, 'Testing that the list of Movie instances has 3 or more instances of a movie') 
-	def test_8(self):
-		conn = sqlite3.connect('Final_project.db')
-		cur = conn.cursor()
-		cur.execute('SELECT * FROM Movies');
-		result = cur.fetchall()
-		self.assertTrue(len(result[2])==8,"Testing that there are 8 columns in the Movies table")
-		conn.close()	
+# class Tests(unittest.TestCase):
+# 	def test_1(self):
+# 		self.assertEqual(type(attempt.movieAppeal()), type('a'), "testing type of return value of movieAppeal method in the Movie Class")
+# 	def test_2(self):
+# 		self.assertEqual(attempt.movieAppeal(), 'This movie is very highly acclaimed', 'Testing the return value of movieAppeal method')	
+# 	def test_3(self):
+# 		self.assertEqual(attempt.title, movie_title, 'testting side effect of the consturctor to see if the instance variable is correct')
+# 	def test_4(self):
+# 		self.assertEqual(attempt.rated, "PG", 'Testing the side effect of the constructor to see if if the instance variable self.rated returns the correct rating')
+# 	def test_5(self):
+# 		self.assertEqual(attempt.getMovieAudience(), 'The Audience likes it more', 'Testing the return value of the getMovideAudience method')
+# 	def test_6(self):
+# 		conn = sqlite3.connect('Final_project.db')
+# 		cur = conn.cursor()
+# 		cur.execute('SELECT * FROM Users');
+# 		result = cur.fetchall()
+# 		self.assertTrue(len(result[1])==3,"Testing that there are 3 columns in the Users database")
+# 		conn.close()
+# 	def test_7(self):
+# 		self.assertTrue(Len(list_of_movie_instances) >= 3, 'Testing that the list of Movie instances has 3 or more instances of a movie') 
+# 	def test_8(self):
+# 		conn = sqlite3.connect('Final_project.db')
+# 		cur = conn.cursor()
+# 		cur.execute('SELECT * FROM Movies');
+# 		result = cur.fetchall()
+# 		self.assertTrue(len(result[2])==8,"Testing that there are 8 columns in the Movies table")
+# 		conn.close()	
 
-## Remember to invoke all your tests...
+# ## Remember to invoke all your tests...
 
-unittest.main(verbosity=2) 
+# unittest.main(verbosity=2) 
